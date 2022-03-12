7cd847046bd4eddfa23e91b0a5903c0eae51f56c
ch-luan2015 <ch.luan2015@gmail.com>
2022-03-12 22:01:53 +0700

    make dashboardSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,19 @@
-import { configureStore, ThunkAction, Action, combineReducers } from "@reduxjs/toolkit";
-import authReducer from "features/auth/authSlice";
-import createSagaMiddleware from "redux-saga";
-import rootSaga from "./rootSaga";
+import { Action, combineReducers, configureStore, ThunkAction } from "@reduxjs/toolkit";
 //Router
 import { connectRouter, routerMiddleware } from "connected-react-router";
+import authReducer from "features/auth/authSlice";
+import dashboardReducer from "features/dashboard/dashboardSlice";
 import { createBrowserHistory } from "history";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./rootSaga";
 
 export const history = createBrowserHistory();
 
 const rootReducer = combineReducers({
   router: connectRouter(history),
   auth: authReducer,
+  dashboard:dashboardReducer
+  
 });
 const sagaMiddleware = createSagaMiddleware();
 
